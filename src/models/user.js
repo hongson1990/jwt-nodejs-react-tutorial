430f92d3e6983129598970463700e04a7fc6b71b
hongson1990 <hongsonndvn2014@gmail.com>
2024-06-16 13:01:04 +0900

    #8.4

--- src/models/user.js
+++ src/models/user.js
@@ -11,15 +11,22 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.belongsTo(models.Group);
+      User.belongsToMany(models.Project, { through: 'Project_User' });
     }
-  };
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    username: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
+  }
+};
+User.init({
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+  username: DataTypes.STRING,
+  address: DataTypes.STRING,
+  sex: DataTypes.STRING,
+  phone: DataTypes.STRING,
+  groupId: DataTypes.INTEGER
+}, {
+  sequelize,
+  modelName: 'User',
+});
+return User;
 };
