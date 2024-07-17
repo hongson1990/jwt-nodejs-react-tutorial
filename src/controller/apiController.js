b0ccb61faac199824302581c85815d556f614bdb
hongson1990 <hongsonndvn2014@gmail.com>
2024-07-18 00:02:01 +0900

    #12.1

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -39,7 +39,8 @@ const handleLogin = async (req, res) => {
     try {
         let data = await handleUserLogin(req.body);
         if (data && data.DT && data.DT.access_token) {
-            res.cookie("jwt", data.DT.access_token, { httpOnly: true, maxAge: 60 * 60 * 1000 });
+            // res.cookie("jwt", data.DT.access_token, { httpOnly: true, maxAge: 60 * 60 * 1000 });
+            res.cookie("jwt", data.DT.access_token, { httpOnly: true });
         }
         return res.status(200).json({
             EM: data.EM,
